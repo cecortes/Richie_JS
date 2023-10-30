38eee7d157c4db13e4cc554df293782e2e6ffd2c
César López Cortés <cesarlopezcortes@hotmail.com>
2023-10-29 21:44:10 -0600

    Clase 4

---
+++ Clase 4/js/algoritmo4.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var numero1 = 6;
+var numero2 = 5;
+
+// Llamar a la función sumar
+SumarNumeros(numero1, numero2);
+// Llamar a la función resta
+// Llamar a la función multiplicar
+// Llamar a la función dividir
+
+// Función sumar
+function SumarNumeros(numero1, numero2) {
+  // Sumar los dos números
+  var resultado = numero1 + numero2;
+
+  // Mostrar el resultado
+  console.log(resultado);
+}
+
+/* --> de acuero al diagrama de bloque algoritmo 4 en azul <-- */
